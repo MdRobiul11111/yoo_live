b3180a425f38f639e439111fc23efd29c5068d86
Tamzid Israk Adol <tamzidisrak12@gmail.com>
2025-08-21 10:20:19 +0600

    Fix gradle issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.yoo_live"
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
